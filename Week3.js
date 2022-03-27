323ce00e36dff37a81470c8f38ee99ca23b3f2d7
JimmyB02143 <najmiseth1@gmail.com>
2022-03-27 21:36:26 +0800

    Add encrypted password code

--- Week3.js
+++ Week3.js
@@ -1,12 +1,32 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://jimmy:abcd1234@sandbox.w2od6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const { faker } = require('@faker-js/faker');
 
+const { faker } = require('@faker-js/faker');
 const firstname = faker.name.firstName(); 
 const lastname =faker.name.lastName();
 const Email = faker.internet.email(); 
 const PhoneNumber = faker.phone.phoneNumber(); 
+
+const bcrypt = require("bcryptjs")
+
+const password = "mypass123"
+const saltRounds = 10
+
+bcrypt.genSalt(saltRounds, function (saltError, salt) {
+	if (saltError) {
+	  throw saltError
+	} else {
+	  bcrypt.hash(password, salt, function(hashError, hash) {
+		if (hashError) {
+		  throw hashError
+		} else {
+		  console.log(hash)
+		  //$2a$10$FEBywZh8u9M0Cec/0mWep.1kXrwKeiWDba6tdKvDfEBjyePJnDT7K
+		}
+	  })
+	}
+  })
 
 client.connect(async err => {
 	if (err) {

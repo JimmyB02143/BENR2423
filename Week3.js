fd9cd0ae0ea54c0f9094eb4989c5bdf2287c8b12
JimmyB02143 <najmiseth1@gmail.com>
2022-04-01 13:39:33 +0800

    Updated

--- Week3.js
+++ Week3.js
@@ -1,3 +1,4 @@
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://jimmy:abcd1234@sandbox.w2od6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -7,45 +8,6 @@ const firstname = faker.name.firstName();
 const lastname =faker.name.lastName();
 const Email = faker.internet.email(); 
 const PhoneNumber = faker.phone.phoneNumber(); 
-
-const UserModel = require("./user.js")
-
-module.exports = {
-  createANewUser: function(username, password, callback) {
-    const newUserDbDocument = new UserModel({
-      username: jimmy23,
-      password: password
-    })
-
-    newUserDbDocument.save(function(error) {
-      if (error) {
-        callback({error: true})
-      } else {
-        callback({success: true})
-      }
-    })
-  }
-}
-
-// const bcrypt = require("bcryptjs")
-
-// const password = "mypass123"
-// const saltRounds = 10
-
-// bcrypt.genSalt(saltRounds, function (saltError, salt) {
-// 	if (saltError) {
-// 	  throw saltError
-// 	} else {
-// 	  bcrypt.hash(password, salt, function(hashError, hash) {
-// 		if (hashError) {
-// 		  throw hashError
-// 		} else {
-// 		  console.log(hash)
-// 		  //$2a$10$FEBywZh8u9M0Cec/0mWep.1kXrwKeiWDba6tdKvDfEBjyePJnDT7K
-// 		}
-// 	  })
-// 	}
-//   })
 
 client.connect(async err => {
 	if (err) {
